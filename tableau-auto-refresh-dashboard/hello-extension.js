11d810400f2ce318b3844d55743087229685e4fb
Denis Tsvetkov <denis.tsvetkov@quadcode.com>
2021-09-03 20:21:05 +0300

    3

---
+++ tableau-auto-refresh-dashboard/hello-extension.js
@@ -0,0 +1,68 @@
+var url = ""
+
+var idInterval = 0;
+var v = null;
+var options = {
+  width: '200em',
+  height: '200em',
+  hideToolbar: true,
+  hideTabs: true
+  };
+
+$(document).ready(function() {
+  
+  setInterval(function() {
+    var newUrl = $("#view-url").val();
+    if (url != newUrl){
+      console.log(newUrl);
+      url = newUrl;
+      initViz();
+    }
+  }, 1000);
+  
+  $("#periods").change(function(){
+    setUpdatePeriod(v);
+  });
+
+  // $("#btn1").click(function() {
+  //   // console.log("btn click");
+  //   try {
+  //     url = $("#view-url").val();
+  //     console.log(url);
+  //     initViz(url);
+  //   } catch(error){
+  //     console.log(error.message);
+  //   }
+  // });
+  
+});
+
+
+
+
+function initViz(){
+  try{
+    if (v) { v.dispose() }
+  }catch(error){
+    console.log(error.message);
+  }
+  try {
+    var divViz = document.getElementById("viz");
+    var _url = url.replace("/#", "");
+    v = new tableau.Viz(divViz, _url, options);
+  }catch(error){
+    console.log(error.message);
+  }
+}
+
+function setUpdatePeriod(v){
+  var period = $("#periods").val()
+  if (idInterval > 0){
+    clearInterval(idInterval);
+  }
+  if (period > 0){
+    idInterval = setInterval(function(){
+      v.refreshDataAsync()
+    }, period * 1000);
+  }
+}
